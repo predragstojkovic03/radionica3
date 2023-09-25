bea42b14b75abaae860dec1d66a9e8aac57785fa
Predrag Stojkovic <predragstojkovic.jagodina@gmail.com>
2023-09-25 20:56:56 +0200

    Finished blabla

---
+++ src/components/SectionNovi/SectionNovi.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './Section.module.css';
+
+function SectionNovi() {
+  return <section className={styles.wrapper}>SectionNovi</section>;
+}
+
+export default SectionNovi;

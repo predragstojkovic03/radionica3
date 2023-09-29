91b5ed3ce58f72ce34f2054f372d8d5d334cd315
Predrag Stojkovic <predragstojkovic.jagodina@gmail.com>
2023-09-29 20:56:34 +0200

    Obrisan section za pokzaivanje

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import HeroSection from '../../components/HeroSection/HeroSection';
 
 import styles from './Homepage.module.css';
-import SectionNovi from '../../components/SectionNovi/SectionNovi';
 
 function Homepage() {
   return (
     <>
       <HeroSection />
-      <SectionNovi />
     </>
   );
 }
